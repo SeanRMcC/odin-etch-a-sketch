f745d0850666ef731f9845bb1316c4b63e99a5bf
Sean <soxfansean@gmail.com>
2023-06-16 12:40:30 -0400

    Add ui elements in html

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ const game = document.querySelector("#grid");
 
 let dim = 16;
 
-for(let i = 0; i < (dim * dim); i++){
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.addEventListener("mouseover", () => {
-        cell.style.backgroundColor = "black";
-    });
-    cell.style.flexBasis = (GAME_SIZE / dim).toString() + "px";
-    cell.style.height = (GAME_SIZE / dim).toString() + "px";
-    game.appendChild(cell);
+function draw(resolution){
+    for(let i = 0; i < (resolution * resolution); i++){
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = "black";
+        });
+        cell.style.flexBasis = (GAME_SIZE / dim).toString() + "px";
+        cell.style.height = (GAME_SIZE / dim).toString() + "px";
+        game.appendChild(cell);
+    }
 }
+
+draw(dim);
