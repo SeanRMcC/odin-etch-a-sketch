171f72b95ba0a15e22e92750fa92a0c846e0b1d4
Sean <soxfansean@gmail.com>
2023-06-16 09:09:20 -0400

    Add mouseover event changing background of cells

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const GAME_SIZE = 512;
 
 const game = document.querySelector("#grid");
 
-let dim = 64;
+let dim = 16;
 
 for(let i = 0; i < (dim * dim); i++){
     const cell = document.createElement("div");
     cell.classList.add("cell");
+    cell.addEventListener("mouseover", () => {
+        cell.style.backgroundColor = "black";
+    });
     cell.style.flexBasis = (GAME_SIZE / dim).toString() + "px";
     cell.style.height = (GAME_SIZE / dim).toString() + "px";
     game.appendChild(cell);
