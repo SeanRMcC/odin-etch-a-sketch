d4743446cdca73f9262d0a8afe62a67fdecbf8e6
Sean <soxfansean@gmail.com>
2023-06-16 13:06:10 -0400

    Add functionality for random & black color buttons

--- script.js
+++ script.js
@@ -2,15 +2,32 @@ const GAME_SIZE = 512;
 
 const game = document.querySelector("#grid");
 const reset = document.querySelector("#reset");
+const blackButton = document.querySelector("#black");
+const randomButton = document.querySelector("#random");
+
 
 let dim = 16;
+let rainbowEnabled = false;
+
+function getColor(){
+    if(rainbowEnabled){
+        const r = parseInt(Math.random() * 255);
+        const g = parseInt(Math.random() * 255);
+        const b = parseInt(Math.random() * 255);
+        console.log(`rgb(${r},${g},${b})`);
+        return `rgb(${r},${g},${b})`;
+    }else{
+        console.log("black");
+        return "black";
+    }
+}
 
 function draw(resolution){
     for(let i = 0; i < (resolution * resolution); i++){
         const cell = document.createElement("div");
         cell.classList.add("cell");
         cell.addEventListener("mouseover", () => {
-            cell.style.backgroundColor = "black";
+            cell.style.backgroundColor = getColor();
         });
         cell.style.flexBasis = (GAME_SIZE / dim).toString() + "px";
         cell.style.height = (GAME_SIZE / dim).toString() + "px";
@@ -26,6 +43,18 @@ function clear(){
 reset.addEventListener("click", () => {
     clear();
     draw(dim);
+});
+
+blackButton.addEventListener("click", () => {
+    clear();
+    draw(dim);
+    rainbowEnabled = false;
+});
+
+randomButton.addEventListener("click", () => {
+    clear();
+    draw(dim);
+    rainbowEnabled = true;
 });
 
 draw(dim);
